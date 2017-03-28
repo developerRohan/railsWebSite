bbd5240779c774c2770fa82cce214431cf3d5b15
MY RAILS <jhambrohan44@gmail.com>
2017-03-28 19:48:17 +0530

    first step

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
+  # get '/articles' => 'articles#index'
+  # get '/articles/new' => 'articles#new'
+  # get '/articles/:id' => 'articles#show'
+  # post '/articles/create' => 'articles#create'
+  # get '/articles/:id/edit' => 'articles#edit'
+  # post '/articles/:id/update' => 'articles#update'
+  # get '/articles/:id/delete' => 'articles#delete'
+  
+
 end
