d10eb25a35d9b9a31d743bcbcffa8eb419ec2877
MY RAILS <jhambrohan44@gmail.com>
2017-03-29 02:00:50 +0530

    added partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
+  get '/' => 'pages#home'
+  get '/about' => 'pages#about'
   # get '/articles' => 'articles#index'
   # get '/articles/new' => 'articles#new'
   # get '/articles/:id' => 'articles#show'
