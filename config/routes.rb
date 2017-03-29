3ecddc417ecfcb481abdabaf55b743b077c8c105
MY RAILS <jhambrohan44@gmail.com>
2017-03-30 00:35:21 +0530

    added styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles
-  get '/' => 'pages#home'
+  root  'pages#home'
   get '/about' => 'pages#about'
+  resources :articles
+
   # get '/articles' => 'articles#index'
   # get '/articles/new' => 'articles#new'
   # get '/articles/:id' => 'articles#show'
