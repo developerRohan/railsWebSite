7a29ae0fa42b287464fc9b5e0e4ab5df2c75231c
MY RAILS <jhambrohan44@gmail.com>
2017-03-30 22:03:30 +0530

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
-
+	validates :username , presence: true ,uniqueness: {case_sensitive: false} , length: {minimum: 3 , maximum: 25}
+  	
+    emailRegex= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+	validates :email , presence: true ,uniqueness: {case_sensitive: false} , length: {minimum: 5 , maximum: 25},
+			                                format: {with: emailRegex}
 
 end
