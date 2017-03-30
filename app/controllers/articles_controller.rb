e01f3f77e526117f32ad09ba59393e7b9de99ebd
MY RAILS <jhambrohan44@gmail.com>
2017-03-30 22:58:07 +0530

    add assciations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,8 +24,10 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
+		#debugger
 		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
+		@article.user = User.first	
 		if @article.save
 			flash[:notice] = "article was  succefully saved"
 			redirect_to article_path(@article)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
-	validates :username , presence: true ,uniqueness: {case_sensitive: false} , length: {minimum: 3 , maximum: 25}
+	has_many :articles
+	validates :username , presence: true ,uniqueness: {case_sensitive: false} ,
+											 length: {minimum: 3 , maximum: 25}
   	
     emailRegex= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
-	validates :email , presence: true ,uniqueness: {case_sensitive: false} , length: {minimum: 5 , maximum: 25},
+	validates :email , presence: true ,uniqueness: {case_sensitive: false} , 
+											length: {minimum: 5 , maximum: 25},
 			                                format: {with: emailRegex}
 
 end
