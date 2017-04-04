4530bf0966c6de69a949c629d26fe7147f0736aa
MY RAILS <jhambrohan44@gmail.com>
2017-04-05 02:57:44 +0530

    added admin control to edit the category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,9 @@
 class CategoriesController < ApplicationController
 
-before_action :require_admin , except: [:index , :show]
+	before_action :require_admin , except: [:index , :show]
 	def index
 		@categories = Category.paginate(page: params[:page] , per_page:5)
-		
+
 	end
 
 	def new 
